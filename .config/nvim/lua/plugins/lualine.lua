6cb1091223331f23a91070ccfb7342ae07f8c41e
z1om3k <z1omek@proton.me>
2024-05-22 22:24:53 -0400

    feat(plugin): Add lualine config

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+        require("lualine").setup {
+            options = {
+                icons_enabled = true,
+                filetype = { colored = false },
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+                disabled_filetypes = {},
+                always_divide_middle = true,
+                theme = "rose-pine",
+            },
+        }
+    end
+}
